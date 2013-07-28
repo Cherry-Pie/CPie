af52aba4d6378e5412dc475d11494f1c35cd6b5f
Yaro <12fcv4@gmail.com>
2013-07-29 00:15:43 +0300

    add Menu class

--- cpie.py
+++ cpie.py
@@ -25,6 +25,17 @@ def main():
     pygame.display.set_caption("HROMD")
     timer = pygame.time.Clock()
 
+    screen.fill((51,51,51))
+    MENU = True
+    menu = Menu()#necessary
+    #menu.set_colors((255,255,255), (0,0,255), (0,0,0))#optional
+    #menu.set_fontsize(64)#optional
+    #menu.set_font('data/couree.fon')#optional
+    #menu.move_menu(100, 99)#optional
+    menu.init(['Start','Options','Quit'], screen)#necessary
+    #menu.move_menu(0, 0)#optional
+    menu.draw()#necessary
+
     mixer.music.load(BG_MUSIC)
     mixer.music.play(0,0.0)
 
@@ -41,27 +52,17 @@ def main():
         "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
         "P                                          P",
         "P                                          P",
+        "P                                          P",
+        "P                                          P",
+        "P                                          P",
+        "P                                          P",
+        "P                                          P",
+        "P                                          P",
+        "P                     P                    P",
+        "P               P              P           P",
+        "P       P                            P     P",
+        "P                                          P",
         "P                                         EP",
-        "P                                  PPPPPPPPP",
-        "P                                          P",
-        "P                    PPPPPPPP              P",
-        "P                                          P",
-        "P    PPPPPPPP                              P",
-        "P                              P           P",
-        "P                              PPP         P",
-        "P                 PPPPPP                   P",
-        "P                                          P",
-        "P         PPPPPPP                          P",
-        "P                                          P",
-        "P                     PPPPPP               P",
-        "P                                          P",
-        "P      PPPPPPPP                            P",
-        "P                                          P",
-        "P                 PPPPPPPPPPP              P",
-        "P                                          P",
-        "P                               PPPPP      P",
-        "P                                          P",
-        "P                                          P",
         "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",]
     # build the level
     for row in level:
@@ -89,51 +90,70 @@ def main():
         #print timedelta
 
         for e in pygame.event.get():
-            if e.type == FISH_EVENT:
-                font = pygame.font.Font(None, 76)
-                text = font.render("YUMMY :3", 1, (123, 204, 201))
-                textpos = text.get_rect(centerx=bg.get_width()/2, centery=100)
-                newBg = pygame.image.load('data/img/bg2.png')
-                bg.blit(newBg, (0,0))
-                bg.blit(text, textpos)
-            if e.type == QUIT:
-                pygame.quit()
-            if e.type == KEYDOWN and e.key == K_ESCAPE:
-                pygame.quit()
-            if e.type == KEYDOWN and e.key == K_UP:
-                up = True
-            if e.type == KEYDOWN and e.key == K_DOWN:
-                down = True
-            if e.type == KEYDOWN and e.key == K_LEFT:
-                left = True
-            if e.type == KEYDOWN and e.key == K_RIGHT:
-                right = True
-            if e.type == KEYDOWN and e.key == K_SPACE:
-                running = True
+            if (MENU):
+                if e.type == KEYDOWN:
+                    if e.key == K_UP:
+                        menu.draw(-1) #here is the Menu class function
+                    if e.key == K_DOWN:
+                        menu.draw(1) #here is the Menu class function
+                    if e.key == K_RETURN:
+                        print menu.get_position()
+                        if menu.get_position() == 0:#here is the Menu class function
+                            MENU = False
+                        if menu.get_position() == 2:#here is the Menu class function
+                            pygame.display.quit()
+                    if e.key == K_ESCAPE:
+                        pygame.quit()
+                if e.type == QUIT:
+                    pygame.quit()
+            else:
+                if e.type == FISH_EVENT:
+                    font = pygame.font.Font(None, 76)
+                    text = font.render("YUMMY :3", 1, (123, 204, 201))
+                    textpos = text.get_rect(centerx=bg.get_width()/2, centery=100)
+                    newBg = pygame.image.load('data/img/bg2.png')
+                    bg.blit(newBg, (0,0))
+                    bg.blit(text, textpos)
+                if e.type == QUIT:
+                    pygame.quit()
+                if e.type == KEYDOWN and e.key == K_ESCAPE:
+                    pygame.quit()
+                if e.type == KEYDOWN and e.key == K_UP:
+                    up = True
+                if e.type == KEYDOWN and e.key == K_DOWN:
+                    down = True
+                if e.type == KEYDOWN and e.key == K_LEFT:
+                    left = True
+                if e.type == KEYDOWN and e.key == K_RIGHT:
+                    right = True
+                if e.type == KEYDOWN and e.key == K_SPACE:
+                    running = True
 
-            if e.type == KEYUP and e.key == K_UP:
-                up = False
-            if e.type == KEYUP and e.key == K_DOWN:
-                down = False
-            if e.type == KEYUP and e.key == K_RIGHT:
-                right = False
-            if e.type == KEYUP and e.key == K_LEFT:
-                left = False
-            if e.type == KEYUP and e.key == K_RIGHT:
-                right = False
+                if e.type == KEYUP and e.key == K_UP:
+                    up = False
+                if e.type == KEYUP and e.key == K_DOWN:
+                    down = False
+                if e.type == KEYUP and e.key == K_RIGHT:
+                    right = False
+                if e.type == KEYUP and e.key == K_LEFT:
+                    left = False
+                if e.type == KEYUP and e.key == K_RIGHT:
+                    right = False
 
         # draw background
         #for y in range(32):
         #    for x in range(32):
         #        screen.blit(bg, (x * 32, y * 32))
-        screen.blit(bg, (0, 0))
+        if not(MENU):
+            screen.blit(bg, (0, 0))
+            camera.update(player)
 
-        camera.update(player)
+            # update player, draw everything else
+            player.update(up, down, left, right, running, platforms, timedelta)
+            for e in entities:
+                screen.blit(e.image, camera.apply(e))
 
-        # update player, draw everything else
-        player.update(up, down, left, right, running, platforms, timedelta)
-        for e in entities:
-            screen.blit(e.image, camera.apply(e))
+
 
         pygame.display.update()
 
@@ -149,20 +169,20 @@ class Camera(object):
         self.state = self.camera_func(self.state, target.rect)
 
 def simple_camera(camera, target_rect):
-	l, t, _, _ = target_rect
-	_, _, w, h = camera
-	return Rect(-l+HALF_WIDTH, -t+HALF_HEIGHT, w, h)
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    return Rect(-l+HALF_WIDTH, -t+HALF_HEIGHT, w, h)
 
 def complex_camera(camera, target_rect):
-	l, t, _, _ = target_rect
-	_, _, w, h = camera
-	l, t, _, _ = -l+HALF_WIDTH, -t+HALF_HEIGHT, w, h
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t, _, _ = -l+HALF_WIDTH, -t+HALF_HEIGHT, w, h
 
-	l = min(0, l)                           # stop scrolling at the left edge
-	l = max(-(camera.width-WIN_WIDTH), l)   # stop scrolling at the right edge
-	t = max(-(camera.height-WIN_HEIGHT), t) # stop scrolling at the bottom
-	t = min(0, t)                           # stop scrolling at the top
-	return Rect(l, t, w, h)
+    l = min(0, l)                           # stop scrolling at the left edge
+    l = max(-(camera.width-WIN_WIDTH), l)   # stop scrolling at the right edge
+    t = max(-(camera.height-WIN_HEIGHT), t) # stop scrolling at the bottom
+    t = min(0, t)                           # stop scrolling at the top
+    return Rect(l, t, w, h)
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self):
@@ -259,6 +279,97 @@ class ExitBlock(Platform):
         self.image = pygame.transform.scale(self.image,(32,32))
         self.image.convert()
         self.rect = Rect(x, y, 32, 32)
+
+class Menu:
+    lista = []
+    pola = []
+    rozmiar_fontu = 32
+    font_path = 'data/font/coders_crux.ttf'
+    font = pygame.font.Font
+    dest_surface = pygame.Surface
+    ilosc_pol = 0
+    kolor_tla = (51,51,51)
+    kolor_tekstu =  (255, 255, 153)
+    kolor_zaznaczenia = (153,102,255)
+    pozycja_zaznaczenia = 0
+    pozycja_wklejenia = (0,0)
+    menu_width = 0
+    menu_height = 0
+
+    class Pole:
+        tekst = ''
+        pole = pygame.Surface
+        pole_rect = pygame.Rect
+        zaznaczenie_rect = pygame.Rect
+
+    def move_menu(self, top, left):
+        self.pozycja_wklejenia = (top,left)
+
+    def set_colors(self, text, selection, background):
+        self.kolor_tla = background
+        self.kolor_tekstu =  text
+        self.kolor_zaznaczenia = selection
+
+    def set_fontsize(self,font_size):
+        self.rozmiar_fontu = font_size
+
+    def set_font(self, path):
+        self.font_path = path
+
+    def get_position(self):
+        return self.pozycja_zaznaczenia
+
+    def init(self, lista, dest_surface):
+        self.lista = lista
+        self.dest_surface = dest_surface
+        self.ilosc_pol = len(self.lista)
+        self.stworz_strukture()
+
+    def draw(self,przesun=0):
+        if przesun:
+            self.pozycja_zaznaczenia += przesun
+            if self.pozycja_zaznaczenia == -1:
+                self.pozycja_zaznaczenia = self.ilosc_pol - 1
+            self.pozycja_zaznaczenia %= self.ilosc_pol
+        menu = pygame.Surface((self.menu_width, self.menu_height))
+        menu.fill(self.kolor_tla)
+        zaznaczenie_rect = self.pola[self.pozycja_zaznaczenia].zaznaczenie_rect
+        pygame.draw.rect(menu,self.kolor_zaznaczenia,zaznaczenie_rect)
+
+        for i in xrange(self.ilosc_pol):
+            menu.blit(self.pola[i].pole,self.pola[i].pole_rect)
+        self.dest_surface.blit(menu,self.pozycja_wklejenia)
+        return self.pozycja_zaznaczenia
+
+    def stworz_strukture(self):
+        przesuniecie = 0
+        self.menu_height = 0
+        self.font = pygame.font.Font(self.font_path, self.rozmiar_fontu)
+        for i in xrange(self.ilosc_pol):
+            self.pola.append(self.Pole())
+            self.pola[i].tekst = self.lista[i]
+            self.pola[i].pole = self.font.render(self.pola[i].tekst, 1, self.kolor_tekstu)
+
+            self.pola[i].pole_rect = self.pola[i].pole.get_rect()
+            przesuniecie = int(self.rozmiar_fontu * 0.2)
+
+            height = self.pola[i].pole_rect.height
+            self.pola[i].pole_rect.left = przesuniecie
+            self.pola[i].pole_rect.top = przesuniecie+(przesuniecie*2+height)*i
+
+            width = self.pola[i].pole_rect.width+przesuniecie*2
+            height = self.pola[i].pole_rect.height+przesuniecie*2
+            left = self.pola[i].pole_rect.left-przesuniecie
+            top = self.pola[i].pole_rect.top-przesuniecie
+
+            self.pola[i].zaznaczenie_rect = (left,top ,width, height)
+            if width > self.menu_width:
+                    self.menu_width = width
+            self.menu_height += height
+        x = self.dest_surface.get_rect().centerx - self.menu_width / 2
+        y = self.dest_surface.get_rect().centery - self.menu_height / 2
+        mx, my = self.pozycja_wklejenia
+        self.pozycja_wklejenia = (x+mx, y+my)
 
 if __name__ == "__main__":
     main()
